1a919b9988ead45fdf76ec0276fa0118a2414c31
yaxon <yaxon@mail.ru>
2019-10-19 12:54:08 +0300

    model Page

---
+++ Models/Page.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class Page<T>
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public List<T> Records { get; set; }
+
+        public Page()
+        {
+            Records = new List<T>();
+        }
+
+        public Page(IEnumerable<T> records)
+        {
+            Records = new List<T>(records);
+        }
+    }
+}
