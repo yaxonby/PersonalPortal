6d33c7652e71399455a6110441df411653188873
yaxon <yaxon@mail.ru>
2019-10-19 12:42:08 +0300

    model

---
+++ Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int PostId { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
